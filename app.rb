7f8fabdf932041be84a49f1bafff2a2ec2a0d710
why <gtrfwhy@gmail.com>
2021-10-25 09:11:05 +0000

    add name

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
   get '/' do
-    "<!DOCTYPE html><html><head></head><body><h1>Hello World</h1></body></html>"
+    "<!DOCTYPE html><html><head></head><body><h1>Hello World -- 王恒宇 202128015029007</h1></body></html>"
   end
 end
 
